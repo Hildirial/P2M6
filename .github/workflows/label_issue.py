ddb0f37efcfd7765ae5e311a75a1840580f765c6
Hildirial <hildirial@gmail.com>
2023-01-27 07:25:48 +0100

    UPDATE

--- .github/workflows/label_issue.py
+++ .github/workflows/label_issue.py
@@ -1,14 +1,17 @@
 from github import Github
+import os
 
 # Initialize the Github client using an access token
 access_token = "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"
 g = Github(access_token)
 
 # Get the repository
-repo = g.get_repo("Hildirial/P2M6")
+repo_name = os.environ["Hildirial"]
+username, reponame = repo_name.split("/")
+repo = g.get_user(username).get_repo(reponame)
 
 # Get the issue by its number
-issue_number = 123
+issue_number = int(os.environ[GITHUB_EVENT_NUMBER])
 issue = repo.get_issue(issue_number)
 
 # Get the labels of the issue

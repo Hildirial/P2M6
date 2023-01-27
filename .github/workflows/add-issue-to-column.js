fd9cf725f6c22bb78fc9f70462cd11b9865c6999
Hildirial <hildirial@gmail.com>
2023-01-27 11:44:38 +0100

    update

--- .github/workflows/add-issue-to-column.js
+++ .github/workflows/add-issue-to-column.js
@@ -1,25 +1,29 @@
-const axios = require("axios");
+const { Octokit } = require("@octokit/rest");
 
-async function addIssueToColumn(octokit, columnName, issueNumber) {
-  // Get the repository name and owner from context
-  const { repository } = context.payload;
-  const owner = repository.owner.login;
-  const repo = repository.name;
+async function addIssueToColumn(columnName, issueNumber, octokit) {
+  try {
+    // Get the project ID
+    const projects = await octokit.projects.listForRepo({
+      owner: process.env.OWNER,
+      repo: process.env.REPO,
+    });
+    const projectId = projects.data[0].id;
 
-  // Get the project ID
-  const projects = await octokit.projects.listForRepo({ owner, repo });
-  const projectId = projects.data[0].id;
+    // Get the column ID by column name
+    const columns = await octokit.projects.listColumns({
+      project_id: projectId,
+    });
+    const columnId = columns.data.find((c) => c.name === columnName).id;
 
-  // Get the column ID by column name
-  const columns = await octokit.projects.listColumns({ project_id: projectId });
-  const columnId = columns.data.find((c) => c.name === columnName).id;
-
-  // Add the issue to the column
-  await octokit.projects.createCard({
-    column_id: columnId,
-    content_id: issueNumber,
-    content_type: "Issue",
-  });
+    // Add the issue to the column
+    await octokit.projects.createCard({
+      column_id: columnId,
+      issue_number: issueNumber,
+    });
+  } catch (error) {
+    console.error(`Error adding issue to column: ${error}`);
+    throw error;
+  }
 }
 
 module.exports = addIssueToColumn;
